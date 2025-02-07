aa4d2316cce5ab3df3d6cde304a8665edce60391
Brenna Meyer <brennameyer9@gmail.com>
2025-02-07 13:31:24 -0500

    Add project files.

---
+++ 20-Brenna Meyer.cpp
@@ -0,0 +1,16 @@
+// 20-Brenna Meyer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Programmer:Brenna Meyer
+// Date: February 7th,2025
+//Requirements: How Much Paint A particular brand of paint covers 340 square feet per gallon.
+// Write a program to determine and report approximately how many gallons of paint will be needed to paint two coats on a 
+// wooden fence that is 6 feet high and 100 feet long.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
